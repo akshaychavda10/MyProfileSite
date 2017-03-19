0300fc8653e155cf51cc3d9f8767cc793a2459b8
akshaychavda10 <akshaychavda10@gmail.com>
2017-03-19 12:14:30 +0530

    Site

---
+++ MyProfileSite/DownloadHttpHandler.ashx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CSASPNETResumeDownload2012
+{
+    /// <summary>
+    /// Summary description for DownloadHttpHandler
+    /// </summary>
+    public class DownloadHttpHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string filePath = ConfigurationManager.AppSettings["FilePath"];
+            Downloader.DownloadFile(context, filePath);
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
